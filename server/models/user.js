a13afb1e50208e4aa8931beedad949ec86e7aeaa
bsdworkin <bsdwork@ilstu.edu>
2017-12-05 07:52:41 -0600

    please work

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var User = mongoose.model('User', {
     email: {
         type: String,
-        trim: true,
+        trim: true
     }
 });
 
--- server/server.js
+++ server/server.js
@@ -4,9 +4,7 @@ var{ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./models/user');
 
-//const path = require('path');
-//const http = require('http');
-//const socketIO = require('socket.io');
+
 var app = express();
 
 //const publicPath = path.join(__dirname, '../public' );
@@ -14,27 +12,6 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.post('/users', (req, res)=>{
-    var user = new User({
-        email: 'something'
-     });
-    
-    user.save().then((doc)=>{
-        res.send(doc);
-    }, (e) => {
-        res.status(400).send(e);
-    });
-});
-
-//app.use(express.static(publicPath));
-
-//var server = http.createServer(app);
-//var io = socketIO(server);
-
-
-//io.on('connection', (socket)=>{
-//    console.log('New user connected');
-//});
 
 app.listen(port, ()=>{
     console.log(`Started up at ${port}`)
