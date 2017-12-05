3d5c076fe0360f8a28e21b6f0747aec3677f5c32
bsdworkin <bsdwork@ilstu.edu>
2017-12-05 06:42:49 -0600

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,31 @@
+    var socket = io();
+
+    socket.on('connect', function (){
+        console.log('Connected to server');
+    });
+
+
+    jQuery('#contactForm').on('submit', function(e){
+         e.preventDefault();
+         var error = "";
+        if($("#exampleInputEmail1").val()==""){
+            error+= "Please enter a email<br>";
+        }
+            
+        if($("#example1").val()==""){
+            error+= "Please enter a name<br>";
+        }
+            
+        if($("#exampleFormControlTextarea1").val()==""){
+            error+= "Please enter a message";
+        }
+            
+        if(error != ""){
+            $("#error").html('<div class="alert alert-dark" role="alert"><p><strong>There were error(s) in your form:</strong><p>' + error + '</div>');
+        }else{
+            $("form").unbind("submit").submit();
+        }
+    });
+
+    
+        
